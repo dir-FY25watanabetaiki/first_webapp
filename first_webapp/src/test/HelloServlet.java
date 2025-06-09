e99b78dcb6b1e09b9b7fc9acd732a85ad46fdc9e
dir-FY25watanabetaiki <taiki.watanabe.a372@dirbato.co.jp>
2025-06-09 16:56:28 +0900

    Lesson15

---
+++ first_webapp/src/test/HelloServlet.java
@@ -0,0 +1,49 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public HelloServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        java.io.PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>はじめてのサーブレット</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("こんにちは、サーブレットの世界へ！！");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // POSTリクエストもGETと同じ処理をする場合
+        doGet(request, response);
+    }
+}
+
